d345c5e29cb8da0be286268cd7a4821844a4a28b
dadvolodvelocity <81975014+dadvolodvelocity@users.noreply.github.com>
2021-09-05 14:31:01 -0400

    adding framework for selenium

---
+++ src/test/java/src/test/java/googleSearch.java
@@ -0,0 +1,29 @@
+package src.test.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class googleSearch {
+
+    SuiteSetUp suite = new SuiteSetUp();
+    WebDriver driver;
+
+    By searchInput = By.name("q");
+    By searchButton = By.xpath("//div[@jsname='VlcLAe']//input[@name='btnK']");
+    By adelleSongLinkText = By.linkText("Adele - Hello - YouTube");
+
+    @Test
+    public void verifyGoogleSearch() {
+        driver = suite.setDriver();
+        driver.get("http://www.google.com");
+        driver.findElement(searchInput).sendKeys("Hello");
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(searchButton));
+        driver.findElement(searchButton).click();
+        Assert.assertTrue("Failed search on google", driver.findElement(adelleSongLinkText).isDisplayed());
+    }
+}
