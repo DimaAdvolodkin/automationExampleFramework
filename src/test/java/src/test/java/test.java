7f2d08c494fb92a0c25e2706bf946ef9748bde99
dadvolodvelocity <81975014+dadvolodvelocity@users.noreply.github.com>
2021-09-05 13:30:50 -0400

    updated framwork

---
+++ src/test/java/src/test/java/test.java
@@ -0,0 +1,22 @@
+package src.test.java;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+
+
+public class test {
+
+    WebDriver driver = new FirefoxDriver();
+
+};
+
+
